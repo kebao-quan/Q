9a3aac59b5737d88f1bb799bc6d4ffc598d7fedf
KayBeeq <55208900+kebao-quan@users.noreply.github.com>
2022-12-06 06:16:40 +0800

    Initial Commit

---
+++ QMessage/QMessage.h
@@ -0,0 +1,8 @@
+﻿// QMessage.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+#include "message.h"
+// TODO: Reference additional headers your program requires here.
